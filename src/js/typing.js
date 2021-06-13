5875a8a8c47013ef48afd1ca52ec47743d3f081e
T. Bold <34991347+tbold@users.noreply.github.com>
2021-06-12 17:11:54 -0700

    Update typing.js

--- src/js/typing.js
+++ src/js/typing.js
@@ -2,7 +2,7 @@ new TypeIt("#typewriter", {
   speed: 50,
   loop: true
 }).pause(750)
-  .type("HR Professional", {delay: 2000})
+  .type("Programs Coordinator", {delay: 2000})
   .delete(null, {delay: 1000})
   .type("Adventurer", {delay: 2000})
   .go();
